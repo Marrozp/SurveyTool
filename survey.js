c5e5050c28f1c2bebdbbc8fcff178a297259a879
Marrozp <71210043+Marrozp@users.noreply.github.com>
2023-09-13 23:11:29 +0200

    Update survey.js

--- survey.js
+++ survey.js
@@ -1,12 +1,14 @@
-console.log("Survey code initiated");
-const rectangle = document.createElement("div");
-rectangle.id = "survey";
-rectangle.text = "Random survey rectangle";
-rectangle.style.backgroundColor = "green";
-rectangle.style.height = "100px";
-rectangle.style.width = "200px";
-rectangle.style.position = "sticky";
-rectangle.style.bottom = "0px";
-rectangle.style.left = "75%";
-rectangle.style.zIndex = "1000";
-document.body.appendChild(rectangle);
+function InjectSurvey() {
+  console.log("Survey code initiated");
+  const rectangle = document.createElement("div");
+  rectangle.id = "survey";
+  rectangle.text = "Random survey rectangle";
+  rectangle.style.backgroundColor = "green";
+  rectangle.style.height = "100px";
+  rectangle.style.width = "200px";
+  rectangle.style.position = "sticky";
+  rectangle.style.bottom = "0px";
+  rectangle.style.left = "75%";
+  rectangle.style.zIndex = "1000";
+  document.body.appendChild(rectangle);
+}
